7bbe508e1dc956b96f24dbbeb19b73bc04c30ff9
rouu04 <rociunon@ucm.es>
2026-01-21 11:34:17 +0100

    Fix privacidad api key github

--- app/src/main/java/com/pastillerodigital/cuidamedpill/controlador/MainActivity.java
+++ app/src/main/java/com/pastillerodigital/cuidamedpill/controlador/MainActivity.java
@@ -37,7 +37,7 @@ public class MainActivity extends AppCompatActivity {
         db = FirebaseFirestore.getInstance();
 
 
-        //probarAddUsuario();
+        probarAddUsuario();
     }
 
     private void probarAddUsuario() {
