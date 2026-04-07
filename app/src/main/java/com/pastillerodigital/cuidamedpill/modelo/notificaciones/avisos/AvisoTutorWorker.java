7a9c9023468a21c587d3f80c232484a3aa3ef817
rouu04 <rociunon@ucm.es>
2026-04-07 18:46:06 +0200

    Fix aviso tutores, check bug misterioso

--- app/src/main/java/com/pastillerodigital/cuidamedpill/modelo/notificaciones/avisos/AvisoTutorWorker.java
+++ app/src/main/java/com/pastillerodigital/cuidamedpill/modelo/notificaciones/avisos/AvisoTutorWorker.java
@@ -33,15 +33,16 @@ public class AvisoTutorWorker extends Worker {
     @NonNull
     @Override
     public Result doWork() {
+        Log.d("AvisoTutorWorker", "En do work");
 
         String idAsistido = getInputData().getString(Constantes.NOTI_INPUT_ID_ASISTIDO);
         String nombreMed = getInputData().getString(Constantes.NOTI_INPUT_NOMBRE_MED);
         String medId = getInputData().getString(Constantes.NOTI_INPUT_IDMED);
         String uidSelf = getInputData().getString(Constantes.ARG_UIDSELF);
-        String aliasU = getInputData().getString(Constantes.NOTI_INPUT_ALIAS_USUARIO);
+        String aliasU = getInputData().getString(Constantes.USUARIO_ALIAS);
         long tiempoProgramado = getInputData().getLong(Constantes.NOTI_INPUT_TIEMPO_PROGRAMADO, 0);
 
-        String[] tutoresArray = getInputData().getStringArray(Constantes.NOTI_INPUT_TUTORES_ARRAY);
+        String[] tutoresArray = getInputData().getStringArray(Constantes.NOTI_INPUT_TUTORES);
         if (tutoresArray == null) return Result.failure();
 
         List<String> tutores = Arrays.asList(tutoresArray);
@@ -60,6 +61,12 @@ public class AvisoTutorWorker extends Worker {
             long margen = 2 * 60 * 1000; //lo que decide si la toma fue tomada o no
             //verificará si hay alguna ingesta registrada entre esas horas
 
+            Log.d("AvisoTutorWorker", "En verifica");
+
+            Date inicio = new Date(tiempoProgramado - margen);
+            Date fin = new Date(tiempoProgramado + margen);
+            /*
+
             // Consultar si existe registro de ingesta dentro del margen
             QuerySnapshot snapshot = Tasks.await(
                     db.collection(Constantes.COLLECTION_USUARIOS)
@@ -67,13 +74,15 @@ public class AvisoTutorWorker extends Worker {
                             .collection(Constantes.COLLECTION_MEDICAMENTOS)
                             .document(medId)
                             .collection(Constantes.COLLECTION_INGESTAS)
-                            .whereGreaterThan(Constantes.ING_FECHAPROGRAMADA, new Date(tiempoProgramado - margen))
-                            .whereLessThan(Constantes.ING_FECHAPROGRAMADA, new Date(tiempoProgramado + margen))
+                            .whereGreaterThan(Constantes.ING_FECHAPROGRAMADA, inicio)
+                            .whereLessThan(Constantes.ING_FECHAPROGRAMADA, fin)
                             .get()
             );
 
+            Log.d("AvisoTutorWorker", "Antes empty");
             if (!snapshot.isEmpty()) {
                 for (QueryDocumentSnapshot document : snapshot) {
+                    Log.d("AvisoTutorWorker", "dentro empty");
                     String estado = document.getString(Constantes.ING_ESTADOINGESTASTR);
                     if (EstadoIngesta.TOMADA.toString().equals(estado)) {
                         return;
@@ -81,13 +90,16 @@ public class AvisoTutorWorker extends Worker {
                 }
             }
 
+             */
+
             // Formatear la hora de la toma programada
             SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm", Locale.getDefault());
             String fechaAviso = sdf.format(new Date(tiempoProgramado));
 
             // Iterar sobre cada tutor
             for (String tutorId : tutores) {
-
+                Log.d("AvisoTutorWorker", "tutor");
+                /*
                 QuerySnapshot existing = Tasks.await(
                         db.collection(Constantes.COLLECTION_USUARIOS)
                                 .document(tutorId)
@@ -100,8 +112,11 @@ public class AvisoTutorWorker extends Worker {
                 );
 
                 if (!existing.isEmpty()) {
+                    Log.d("AvisoTutorWorker", "Existe para " + tutorId);
                     continue; // ya existe
                 }
+
+                 */
 
                 // Crear aviso
                 Aviso aviso = new Aviso(
